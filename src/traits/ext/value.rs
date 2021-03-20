5ab13bd73ad95e5aaeb8e30b7de219f17fbb4b6d
l1h3r <devin.turner09@gmail.com>
2021-03-19 22:49:37 -0700

    paste

--- src/traits/ext/value.rs
+++ src/traits/ext/value.rs
@@ -1,5 +1,9 @@
 use wasmlib::*;
 
+type ScBytes = Vec<u8>;
+type ScInt64 = i64;
+type ScString = String;
+
 /// A common interface for smart contract values.
 pub trait Value {
   type Primitive: ?Sized;
@@ -8,52 +12,6 @@ pub trait Value {
   type ContainerMut: ContainerMut<Value = Self>;
   type ContainerArray: ContainerArray<Value = Self::Container>;
   type ContainerArrayMut: ContainerArrayMut<Value = Self::ContainerMut>;
-}
-
-macro_rules! impl_Value {
-  (
-    $ident:ty,
-    $primitive:ty,
-    $container:ty,
-    $container_mut:ty,
-    $container_array:ty,
-    $container_array_mut:ty
-  ) => {
-    impl Value for $ident {
-      type Primitive = $primitive;
-
-      type Container = $container;
-      type ContainerMut = $container_mut;
-      type ContainerArray = $container_array;
-      type ContainerArrayMut = $container_array_mut;
-    }
-  };
-  ($((
-    $ident:ty,
-    $primitive:ty,
-    $container:ty,
-    $container_mut:ty,
-    $container_array:ty,
-    $container_array_mut:ty
-  ),)+) => {
-    $(
-      impl_Value!($ident, $primitive, $container, $container_mut, $container_array, $container_array_mut);
-    )+
-  };
-}
-
-impl_Value! {
-  (ScAddress, [u8; 33], ScImmutableAddress, ScMutableAddress, ScImmutableAddressArray, ScMutableAddressArray),
-  (ScAgentId, [u8; 37], ScImmutableAgentId, ScMutableAgentId, ScImmutableAgentIdArray, ScMutableAgentIdArray),
-  (Vec<u8>, Vec<u8>, ScImmutableBytes, ScMutableBytes, ScImmutableBytesArray, ScMutableBytesArray),
-  (ScChainId, [u8; 33], ScImmutableChainId, ScMutableChainId, ScImmutableChainIdArray, ScMutableChainIdArray),
-  (ScColor, [u8; 32], ScImmutableColor, ScMutableColor, ScImmutableColorArray, ScMutableColorArray),
-  (ScContractId, [u8; 37], ScImmutableContractId, ScMutableContractId, ScImmutableContractIdArray, ScMutableContractIdArray),
-  (ScHash, [u8; 32], ScImmutableHash, ScMutableHash, ScImmutableHashArray, ScMutableHashArray),
-  (ScHname, u32, ScImmutableHname, ScMutableHname, ScImmutableHnameArray, ScMutableHnameArray),
-  (i64, i64, ScImmutableInt64, ScMutableInt64, ScImmutableInt64Array, ScMutableInt64Array),
-  (ScRequestId, [u8; 34], ScImmutableRequestId, ScMutableRequestId, ScImmutableRequestIdArray, ScMutableRequestIdArray),
-  (String, String, ScImmutableString, ScMutableString, ScImmutableStringArray, ScMutableStringArray),
 }
 
 // =============================================================================
@@ -70,55 +28,6 @@ pub trait Container {
   fn get(&self) -> Self::Value;
 }
 
-macro_rules! impl_Container {
-  ($ident:ident, $value:ty) => {
-    impl Container for $ident {
-      type Value = $value;
-
-      fn has(&self) -> bool {
-        self.exists()
-      }
-
-      fn get(&self) -> Self::Value {
-        self.value()
-      }
-    }
-  };
-  ($(($ident:ident, $value:ty),)+) => {
-    $(
-      impl_Container!($ident, $value);
-    )+
-  };
-}
-
-impl_Container! {
-  (ScImmutableAddress, ScAddress),
-  (ScImmutableAgentId, ScAgentId),
-  (ScImmutableBytes, Vec<u8>),
-  (ScImmutableChainId, ScChainId),
-  (ScImmutableColor, ScColor),
-  (ScImmutableContractId, ScContractId),
-  (ScImmutableHash, ScHash),
-  (ScImmutableHname, ScHname),
-  (ScImmutableInt64, i64),
-  (ScImmutableRequestId, ScRequestId),
-  (ScImmutableString, String),
-}
-
-impl_Container! {
-  (ScMutableAddress, ScAddress),
-  (ScMutableAgentId, ScAgentId),
-  (ScMutableBytes, Vec<u8>),
-  (ScMutableChainId, ScChainId),
-  (ScMutableColor, ScColor),
-  (ScMutableContractId, ScContractId),
-  (ScMutableHash, ScHash),
-  (ScMutableHname, ScHname),
-  (ScMutableInt64, i64),
-  (ScMutableRequestId, ScRequestId),
-  (ScMutableString, String),
-}
-
 // =============================================================================
 // =============================================================================
 
@@ -126,45 +35,6 @@ impl_Container! {
 pub trait ContainerMut: Container {
   /// Sets a new inner [`value`][`Container::Value`].
   fn set(&self, value: &Self::Value);
-}
-
-macro_rules! impl_ContainerMut {
-  ($ident:ident, $value:ty) => {
-    impl ContainerMut for $ident {
-      fn set(&self, value: &Self::Value) {
-        self.set_value(value);
-      }
-    }
-  };
-  ($(($ident:ident, $value:ty),)+) => {
-    $(
-      impl_ContainerMut!($ident, $value);
-    )+
-  };
-}
-
-impl_ContainerMut! {
-  (ScMutableAddress, ScAddress),
-  (ScMutableAgentId, ScAgentId),
-  (ScMutableBytes, Vec<u8>),
-  (ScMutableChainId, ScChainId),
-  (ScMutableColor, ScColor),
-  (ScMutableContractId, ScContractId),
-  (ScMutableHash, ScHash),
-  (ScMutableRequestId, ScRequestId),
-  (ScMutableString, String),
-}
-
-impl ContainerMut for ScMutableHname {
-  fn set(&self, value: &Self::Value) {
-    self.set_value(value.clone());
-  }
-}
-
-impl ContainerMut for ScMutableInt64 {
-  fn set(&self, value: &Self::Value) {
-    self.set_value(*value);
-  }
 }
 
 // =============================================================================
@@ -192,8 +62,50 @@ pub trait ContainerArray {
   }
 }
 
-macro_rules! impl_ContainerArray {
-  ($ident:ident, $value:ty, $fn:ident) => {
+// =============================================================================
+// =============================================================================
+
+/// A common interface for mutable arrays of value containers.
+pub trait ContainerArrayMut: ContainerArray {
+  fn erase(&self);
+}
+
+// =============================================================================
+// =============================================================================
+
+macro_rules! impl_ScValue {
+  (@value, $ident:ident, $primitive:ty) => {
+    paste::paste! {
+      impl Value for [<Sc $ident>] {
+        type Primitive = $primitive;
+        type Container = [<ScImmutable $ident>];
+        type ContainerMut = [<ScMutable $ident>];
+        type ContainerArray = [<ScImmutable $ident Array>];
+        type ContainerArrayMut = [<ScMutable $ident Array>];
+      }
+    }
+  };
+  (@container, $ident:ident, $value:ty) => {
+    impl Container for $ident {
+      type Value = $value;
+
+      fn has(&self) -> bool {
+        self.exists()
+      }
+
+      fn get(&self) -> Self::Value {
+        self.value()
+      }
+    }
+  };
+  (@container_mut, $ident:ident, $value:ty) => {
+    impl ContainerMut for $ident {
+      fn set(&self, value: &Self::Value) {
+        self.set_value(value);
+      }
+    }
+  };
+  (@container_array, $ident:ident, $value:ty, $fn:ident) => {
     impl ContainerArray for $ident {
       type Value = $value;
 
@@ -206,76 +118,68 @@ macro_rules! impl_ContainerArray {
       }
     }
   };
-  ($(($ident:ident, $value:ty, $fn:ident),)+) => {
-    $(
-      impl_ContainerArray!($ident, $value, $fn);
-    )+
-  };
-}
-
-// TODO: Support maps
-impl_ContainerArray! {
-  (ScImmutableAddressArray, ScImmutableAddress, get_address),
-  (ScImmutableAgentIdArray, ScImmutableAgentId, get_agent_id),
-  (ScImmutableBytesArray, ScImmutableBytes, get_bytes),
-  (ScImmutableChainIdArray, ScImmutableChainId, get_chain_id),
-  (ScImmutableColorArray, ScImmutableColor, get_color),
-  (ScImmutableContractIdArray, ScImmutableContractId, get_contract_id),
-  (ScImmutableHashArray, ScImmutableHash, get_hash),
-  (ScImmutableHnameArray, ScImmutableHname, get_hname),
-  (ScImmutableInt64Array, ScImmutableInt64, get_int64),
-  (ScImmutableRequestIdArray, ScImmutableRequestId, get_request_id),
-  (ScImmutableStringArray, ScImmutableString, get_string),
-}
-
-// TODO: Support maps
-impl_ContainerArray! {
-  (ScMutableAddressArray, ScMutableAddress, get_address),
-  (ScMutableAgentIdArray, ScMutableAgentId, get_agent_id),
-  (ScMutableBytesArray, ScMutableBytes, get_bytes),
-  (ScMutableChainIdArray, ScMutableChainId, get_chain_id),
-  (ScMutableColorArray, ScMutableColor, get_color),
-  (ScMutableContractIdArray, ScMutableContractId, get_contract_id),
-  (ScMutableHashArray, ScMutableHash, get_hash),
-  (ScMutableHnameArray, ScMutableHname, get_hname),
-  (ScMutableInt64Array, ScMutableInt64, get_int64),
-  (ScMutableRequestIdArray, ScMutableRequestId, get_request_id),
-  (ScMutableStringArray, ScMutableString, get_string),
-}
-
-// =============================================================================
-// =============================================================================
-
-pub trait ContainerArrayMut: ContainerArray {
-  fn erase(&self);
-}
-
-macro_rules! impl_ContainerArrayMut {
-  ($ident:ident) => {
+  (@container_array_mut, $ident:ident) => {
     impl ContainerArrayMut for $ident {
       fn erase(&self) {
         self.clear();
       }
     }
   };
-  ($($ident:ident,)+) => {
+  (@core, $ident:ident, $primitive:ty, $getter:ident) => {
+    impl_ScValue!(@value, $ident, $primitive);
+
+    paste::paste! {
+      impl_ScValue!(@container, [<ScImmutable $ident>], [<Sc $ident>]);
+      impl_ScValue!(@container, [<ScMutable $ident>], [<Sc $ident>]);
+      // impl_ScValue!(@container_mut, [<ScMutable $ident>], [<Sc $ident>]);
+      impl_ScValue!(@container_array, [<ScImmutable $ident Array>], [<ScImmutable $ident>], $getter);
+      impl_ScValue!(@container_array, [<ScMutable $ident Array>], [<ScMutable $ident>], $getter);
+      impl_ScValue!(@container_array_mut, [<ScMutable $ident Array>]);
+    }
+  };
+  (Hname, $primitive:ty, $getter:ident) => {
+    impl_ScValue!(@core, Hname, $primitive, $getter);
+  };
+  (Int64, $primitive:ty, $getter:ident) => {
+    impl_ScValue!(@core, Int64, $primitive, $getter);
+  };
+  ($ident:ident, $primitive:ty, $getter:ident) => {
+    impl_ScValue!(@core, $ident, $primitive, $getter);
+
+    paste::paste! {
+      impl_ScValue!(@container_mut, [<ScMutable $ident>], [<Sc $ident>]);
+    }
+  };
+  ($(($ident:ident, $primitive:ty, $getter:ident),)+) => {
     $(
-      impl_ContainerArrayMut!($ident);
+      impl_ScValue!($ident, $primitive, $getter);
     )+
   };
 }
 
 // TODO: Support maps
-impl_ContainerArrayMut! {
-  ScMutableAddressArray,
-  ScMutableAgentIdArray,
-  ScMutableBytesArray,
-  ScMutableChainIdArray,
-  ScMutableColorArray,
-  ScMutableContractIdArray,
-  ScMutableHashArray,
-  ScMutableHnameArray,
-  ScMutableInt64Array,
-  ScMutableRequestIdArray,
-  ScMutableStringArray,
+impl_ScValue! {
+  (Address, [u8; 33], get_address),
+  (AgentId, [u8; 37], get_agent_id),
+  (Bytes, Vec<u8>, get_bytes),
+  (ChainId, [u8; 33], get_chain_id),
+  (Color, [u8; 32], get_color),
+  (ContractId, [u8; 37], get_contract_id),
+  (Hash, [u8; 32], get_hash),
+  (Hname, u32, get_hname),
+  (Int64, i64, get_int64),
+  (RequestId, [u8; 34], get_request_id),
+  (String, String, get_string),
+}
+
+impl ContainerMut for ScMutableHname {
+  fn set(&self, value: &Self::Value) {
+    self.set_value(value.clone());
+  }
+}
+
+impl ContainerMut for ScMutableInt64 {
+  fn set(&self, value: &Self::Value) {
+    self.set_value(*value);
+  }
 }
